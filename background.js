ea94c55dcfa2cab1f19df3642a6adc1002b3efb5
allen_tien <allen_tien@hiiir.com>
2016-11-29 14:11:26 +0800

    [core #8] trackPixelPlugin : add module track pixel setup wizard

--- background.js
+++ background.js
@@ -1,1 +1,22 @@
 /* life in the browser  */
+(function() {
+
+  TrackPixelSetupWizard.init();
+
+  chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+
+    var action = message.action;
+
+    switch(action) {
+
+      case "showSetupWizard":
+        sendResponse(TrackPixelSetupWizard.getCurrentStep());
+        break;
+
+      case "process":
+        var isDone = TrackPixelSetupWizard.process(message);
+        sendResponse(isDone);
+        break;
+    }
+  });
+}());
